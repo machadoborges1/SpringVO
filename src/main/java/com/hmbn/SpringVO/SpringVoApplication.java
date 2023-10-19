62846f1e6069bfddc78ca44b32bbabd82d24ca06
Humberto <hummberton@gmail.com>
2023-10-19 16:59:06 -0300

    passando o projeto anterior para padrão VO

---
+++ src/main/java/com/hmbn/SpringVO/SpringVoApplication.java
@@ -0,0 +1,13 @@
+package com.hmbn.SpringVO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmbn/SpringVO/SpringVoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmbn.SpringVO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
