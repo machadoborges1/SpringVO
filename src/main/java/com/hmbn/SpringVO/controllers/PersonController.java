43097944537422bee2dc82a61feafd994e967b61
Humberto <hummberton@gmail.com>
2023-10-30 15:43:24 -0300

    migrations

--- src/main/java/com/hmbn/SpringVO/controllers/PersonController.java
+++ src/main/java/com/hmbn/SpringVO/controllers/PersonController.java
@@ -18,36 +18,36 @@ import com.hmbn.SpringVO.data.vo.v1.PersonVO;
 import com.hmbn.SpringVO.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
-	
+
 	@Autowired
 	private PersonServices service;
-	
+
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
-	
+
 	@GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
-	
+
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
-	
+
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
-	
-	
+
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		service.delete(id);
